cd5c3dd231c7131b61dd2f58277528195291db03
codeunifier <eva15023@byui.edu>
2018-09-10 20:25:15 -0700

    prevent infinite loop

--- routers/api.js
+++ routers/api.js
@@ -20,51 +20,51 @@ router.post('/', function (req, res, next) {
         res.send(req.body.challenge);
     } else if (req.body.type == 'event_callback') {
         var e = req.body.event;
-        if (e.type == 'message' && e.channel_type == 'im') {
-            console.log('Querying for: ' + e.text);
-            request("https://api.magicthegathering.io/v1/cards?name=" + e.text, {json: true}, function (rErr, rRes, rBody) {
-                if (rErr) {
-                    console.log(rErr);
-                    res.send(rErr);
-                    return;
-                }
+        // if (e.type == 'message' && e.channel_type == 'im') {
+        //     console.log('Querying for: ' + e.text);
+        //     request("https://api.magicthegathering.io/v1/cards?name=" + e.text, {json: true}, function (rErr, rRes, rBody) {
+        //         if (rErr) {
+        //             console.log(rErr);
+        //             res.send(rErr);
+        //             return;
+        //         }
 
-                var postBody = {
-                    token: oauth_token,
-                    channel: e.channel,
-                    text: 'Returning search for "' + e.text + "'",
-                    // attachments: [
-                    //     {
-                    //         "pretext": rBody.cards[0].name,
-                    //         "image_url": rBody.cards[0].imageUrl
-                    //     }
-                    // ]
-                }
+        //         var postBody = {
+        //             token: oauth_token,
+        //             channel: e.channel,
+        //             text: 'Returning search for "' + e.text + "'",
+        //             // attachments: [
+        //             //     {
+        //             //         "pretext": rBody.cards[0].name,
+        //             //         "image_url": rBody.cards[0].imageUrl
+        //             //     }
+        //             // ]
+        //         }
 
-                console.log(postBody);
+        //         console.log(postBody);
 
-                var opts = {
-                    url: "https://slack.com/api/chat.postMessage",
-                    method: "POST",
-                    json: true,
-                    formData: postBody
-                }
+        //         var opts = {
+        //             url: "https://slack.com/api/chat.postMessage",
+        //             method: "POST",
+        //             json: true,
+        //             formData: postBody
+        //         }
 
-                request(opts, function (mErr, mRes, mBody) {
-                    if (mErr) {
-                        console.log(mErr);
-                        res.send(mErr);
-                        return;
-                    }
-                    console.log('Message sent');
-                    // console.log(mRes);
-                    console.log(mBody);
-                    res.send(200);
-                });
-            });
-        } else {
-            res.send(202); //Accepted
-        }
+        //         request(opts, function (mErr, mRes, mBody) {
+        //             if (mErr) {
+        //                 console.log(mErr);
+        //                 res.send(mErr);
+        //                 return;
+        //             }
+        //             console.log('Message sent');
+        //             // console.log(mRes);
+        //             console.log(mBody);
+        //             res.send(200);
+        //         });
+        //     });
+        // } else {
+        //     res.send(202); //Accepted
+        // }
     } else {
         res.send(202); //Accepted
     }
